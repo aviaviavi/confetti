dd8527919accf3fa60dadca854f4b1e224ccfd40
aviaviavi <avipress@gmail.com>
2017-07-08 16:54:18 -0700

    removing template tests

--- test/Main.hs
+++ test/Main.hs
@@ -2,37 +2,37 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.SmallCheck
 
-import Lib (inc)
+import Lib
 
 main :: IO ()
 main = defaultMain $ testGroup "all-tests" tests
 
 tests :: [TestTree]
-tests =
-  [ testGroup "SmallCheck" scTests
-  , testGroup "Unit tests" huTests
-  ]
+tests = []
+  -- [ testGroup "SmallCheck" scTests
+  -- , testGroup "Unit tests" huTests
+  -- ]
 
-scTests :: [TestTree]
-scTests =
-  [ testProperty "inc == succ" prop_succ
-  , testProperty "inc . negate == negate . pred" prop_pred
-  ]
+-- scTests :: [TestTree]
+-- scTests =
+--   [ testProperty "inc == succ" prop_succ
+--   , testProperty "inc . negate == negate . pred" prop_pred
+--   ]
 
-huTests :: [TestTree]
-huTests =
-  [ testCase "Increment below TheAnswer" case_inc_below
-  , testCase "Decrement above TheAnswer" case_dec_above
-  ]
+-- huTests :: [TestTree]
+-- huTests =
+--   [ testCase "Increment below TheAnswer" case_inc_below
+--   , testCase "Decrement above TheAnswer" case_dec_above
+--   ]
 
-prop_succ :: Int -> Bool
-prop_succ n = inc n == succ n
+-- prop_succ :: Int -> Bool
+-- prop_succ n = inc n == succ n
 
-prop_pred :: Int -> Bool
-prop_pred n = inc (negate n) == negate (pred n)
+-- prop_pred :: Int -> Bool
+-- prop_pred n = inc (negate n) == negate (pred n)
 
-case_inc_below :: Assertion
-case_inc_below = inc 41 @?= (42 :: Int)
+-- case_inc_below :: Assertion
+-- case_inc_below = inc 41 @?= (42 :: Int)
 
-case_dec_above :: Assertion
-case_dec_above = negate (inc (negate 43)) @?= (42 :: Int)
+-- case_dec_above :: Assertion
+-- case_dec_above = negate (inc (negate 43)) @?= (42 :: Int)
