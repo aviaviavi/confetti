3b9adca990ecc78ddf7c95f81412a294755bd49f
aviaviavi <avipress@gmail.com>
2017-07-09 14:15:16 -0700

    docs

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Data.Version       (showVersion)
 import           Lib
+import           Paths_confetti     (version)
 import           System.Environment
 import           System.Exit
 import           Text.Printf
 
 import qualified Data.Text          as T
 
+-- Structure for our command line arguments
 data MainArgs = MainArgs
   { groupName   :: String
   , variantName :: String
   } deriving (Show)
 
+-- Parse our command line arguments for a confetti command,
+-- or display some information and exit
 parseMainArgs :: [String] -> IO MainArgs
 parseMainArgs [g, v] = return (MainArgs g v)
 parseMainArgs a =
   let usage = "Usage: `confetti $group_name $variant_name`"
+      showHelp = (putStrLn usage >> exitSuccess)
+      confettiVersion = putStrLn ("confetti " ++ showVersion version) >> exitSuccess
   in case a of
-       ["-h"] -> putStrLn usage >> exitSuccess
-       _      -> printFail usage >> exitWith (ExitFailure 1)
+       ["-h"]        -> showHelp
+       ["--help"]    -> showHelp
+       ["-v"]        -> confettiVersion
+       ["--version"] -> confettiVersion
+       _             -> printFail usage >> exitWith (ExitFailure 1)
 
 main :: IO ()
 main = do
@@ -29,6 +39,8 @@ main = do
   group <- parseGroup specPath (T.pack $ groupName parsed)
   either (printFail . show) (runSpec (variantName parsed)) group
 
+-- Run our configuration spec. Prints whatever error message we get back,
+-- or a success message
 runSpec :: ConfigVariant -> ConfigGroup -> IO ()
 runSpec variant group =
   applySpec (ConfigSpec group variant) >>= \maybeError ->
--- src/Lib.hs
+++ src/Lib.hs
@@ -19,6 +19,7 @@ import           Text.Printf
 
 import qualified Data.Text             as T
 
+-- Errors from parsing .confetti.yml
 data ParseError a = ConfettiYamlNotFound | GroupNotFound a | ConfettiYamlInvalid a deriving (Generic)
 
 instance (Show a) => Show (ParseError a) where
@@ -26,6 +27,7 @@ instance (Show a) => Show (ParseError a) where
   show (GroupNotFound a) = printf "No match for group %s found in your ~/.confetti.yml: " (show a)
   show ConfettiYamlNotFound = "No confetti spec file found! See https://github.com/aviaviavi/confetti if you need help setting one up"
 
+-- Errors from applying our config spec
 newtype ApplyError a = VariantsMissing [a]
 instance (Show a) => Show (ApplyError a)  where
   show (VariantsMissing a) = "Couldn't find one or more of your variant files to use: " ++ show a
@@ -36,7 +38,6 @@ data ConfigGroup = ConfigGroup
   { name    :: T.Text
   , targets :: [FilePath]
   } deriving (Show, Generic)
-
 
 newtype ParsedSpecFile = ParsedSpecFile
   { groups :: [ConfigGroup]
@@ -52,7 +53,9 @@ data ConfigSpec = ConfigSpec
   , configVariant :: ConfigVariant
   }
 
-parseGroup:: FilePath -> T.Text -> IO (Either (ParseError T.Text) ConfigGroup)
+-- Given a yaml file and a group name, parse the group into a ConfigGroup, or
+-- a ParseError
+parseGroup :: FilePath -> T.Text -> IO (Either (ParseError T.Text) ConfigGroup)
 parseGroup specFile groupName =
   doesFileExist specFile >>= \exists -> parseGroup' exists
   where
@@ -65,22 +68,28 @@ parseGroup specFile groupName =
           (`findGroup` groupName)
           eitherParsed
 
+-- Finds a given group in a parsed spec file. Returns a GroupNotFound if the group
+-- is missing
 findGroup :: ParsedSpecFile -> T.Text -> IO  (Either (ParseError T.Text) ConfigGroup)
 findGroup spec groupName =
   sequence $ maybe (Left $ GroupNotFound groupName) (Right . expandPathsForGroup) $
   find (\g -> name g == groupName) (groups spec)
 
+-- Replaces ~ with the value of $HOME for all files in the group
 expandPathsForGroup :: ConfigGroup -> IO ConfigGroup
 expandPathsForGroup confGroup =
   mapM absolutePath (targets confGroup) >>= \expanded ->
     return $ confGroup {targets = expanded}
 
+-- If a target config file is _not_ a symlink,
+-- make a backup before we swap out the config
 backUpIfNonSymLink :: FilePath -> IO ()
 backUpIfNonSymLink file = do
   exists <- doesFileExist file
   isLink <- if exists then pathIsSymbolicLink file else return False
   when (exists && not isLink) $ createBackup file
 
+-- Backs up a file, eg config.json -> config.json.$time.backup
 createBackup :: FilePath -> IO ()
 createBackup file =
   let newName =
@@ -94,9 +103,16 @@ removeIfExists fileName = removeFile fileName `catch` handleExists
           | isDoesNotExistError e = return ()
           | otherwise = throwIO e
 
+-- Given a list of variant configs, returns the variants that
+-- do not exist
 filterMissingVariants :: [ConfigVariant] -> IO [FilePath]
 filterMissingVariants = filterM (fmap not . doesFileExist)
 
+-- This is where the actual config swapping happens. For every ConfigTarget,
+-- creates a symlink from the target -> variant.
+-- For instance, for target = ~/.aws/credentials, and variant "work",
+-- the link created would be
+-- ~/.aws/credentials -> ~/.aws/work.credentials
 linkTargets :: ConfigVariant -> [ConfigTarget] -> IO ()
 linkTargets variant confTargets =
   let variantPaths = constructFullVariantPaths variant confTargets
@@ -104,7 +120,8 @@ linkTargets variant confTargets =
     (\pair -> createSymbolicLink (fst pair) (snd pair :: FilePath))
     (zip variantPaths confTargets)
 
-
+-- For a given variant and list of targets, construct the full paths of all
+-- variants
 constructFullVariantPaths :: ConfigVariant -> [ConfigTarget] -> [FilePath]
 constructFullVariantPaths variant =
   map
@@ -112,6 +129,8 @@ constructFullVariantPaths variant =
        Data.List.Utils.join "/" (init (split "/" t)) ++
        "/" ++ variant ++ "." ++ last (split "/" t))
 
+-- Run the spec! Every target will be a symlink to the variant
+-- file
 applySpec :: ConfigSpec -> IO (Maybe (ApplyError T.Text))
 applySpec spec = do
   let groupTargets = targets $ configGroup spec
@@ -128,13 +147,16 @@ applySpec spec = do
       return Nothing
     else return $ Just (VariantsMissing (map T.pack missingVariants))
 
+-- Expands ~ to $HOME in a path
 absolutePath :: FilePath -> IO FilePath
 absolutePath path = do
   home <- getHomeDirectory
   return $ replace "~" home path
 
+-- Prints green
 printSuccess :: String -> IO ()
 printSuccess s = putStrLn $ "\x1b[32m" ++  s
 
+-- Prints red
 printFail :: String -> IO ()
 printFail s = putStrLn $ "\x1b[31m" ++  s
